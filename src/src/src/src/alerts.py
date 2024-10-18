dba4fbd3d8a28b439fbc93ae0153566fb4595b41
bindu sree <153990216+bindu-suvala@users.noreply.github.com>
2024-10-18 21:30:30 +0530

    Delete src/src/src directory

--- src/src/src/src/alerts.py
+++
@@ -1,9 +0,0 @@
-class AlertSystem:
-    def __init__(self):
-        self.thresholds = {
-            'temp': 35  # degrees Celsius
-        }
-
-    def check_alerts(self, data):
-        if data['temp'] > self.thresholds['temp']:
-            print(f"Alert: {data['city']} has exceeded temperature threshold with {data['temp']}°C")
